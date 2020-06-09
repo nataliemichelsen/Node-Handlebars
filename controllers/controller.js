62b20ebf8dc15991b951dcf92aafdd58ce45764e
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-06-09 16:02:03 -0600

    Outline & Psuedo 3

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,8 @@
+// require
+const mysql = require("mysql");
+const path = require("path");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
 // 
--- db/seed.sql
+++ db/seed.sql
@@ -2,3 +2,13 @@
 USE db_burger;
 
 -- // insert into statements
+INSERT INTO department ( department_name )
+VALUES ("");
+
+
+INSERT INTO role ( title, salary, department_id )
+VALUES (""), (""), ("");
+
+
+INSERT INTO employee ( first_name, last_name, role_id, manager_id )
+VALUES (""), (""), (""), ("");
