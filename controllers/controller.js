950e0e735d6109df9ac352eeb8c501f3a8cacffc
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-20 16:36:19 -0600

    improving controller.js 2 0f 2

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,36 +7,62 @@ const burger = require("../models/app")
 // define all routers (get, put, post - one each) >>>>
 
 // get (get all)
-router.get("/burgers", function(req, res) {
-  burger.selectAll(function(data) {
-    
-    });
+router.get("/burgers", function (req, res) {
+  burger.selectAll(function (data) {
+    // set new variable for table data
+    let x = {
+      burgers: data
+    };
+    // render index & new variable
+    res.render('index', x)
   });
+});
 
 // put (update)
-router.put("/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function (req, res) {
   // setting params for new variable
-    let x = req.params.id;
-    burger.updateOne(
-      // set UpdateOne status
-      {
-        eaten: true
-      },
-      // calling data in new function for variable x
-      x, function(data) {
-        // return status
-        res.sendStatus(200);
-      }
-    );
-  });
+  let x = req.params.id;
+  burger.updateOne(
+    // set UpdateOne status
+    {
+      eaten: true
+    },
+    // calling data in new function for variable x
+    x, function (data) {
+      // return status
+      res.sendStatus(200);
+    }
+  );
+});
 
 // post (add)
-router.post("/burgers/create", function(req, res) {
-
-  });
+router.post("/burgers/create", function (req, res) {
+  // calling string data from table
+  burger.insertOne(
+    [
+      'burger_name',
+    ],
+    [
+      req.body.burger_name(),
+    ],
+    // operating function
+    function (data) {
+      // return status
+      res.sendStatus(200);
+    });
+});
 
 // delete (delete selected)
-router.delete()
+router.delete('/burgers/:id', function (req, res) {
+  // setting params for new variable
+  let x = req.params.id;
+  // calling data in new function for variable x
+  burger.deleteOne(id, function (data) {
+    // return status
+    res.sendStatus(200);
+  })
+});
+
 module.exports = router;
 
 
