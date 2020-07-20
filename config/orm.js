add67639d91a315b0e5d0f4ef21b78510276c203
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-20 13:55:03 -0600

    updating orm 1 of 2

--- config/orm.js
+++ config/orm.js
@@ -1,80 +1,32 @@
 // require connection.js
 const connection = require("./connection");
 
-// set orm variable to convert data between languages
-// set parameters for all options (select, insert, update)
-// same functions as defined on script.js >> export to script.js
-function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
-  }
-  
-  function objToSql(ob) {
-    // column1=value, column2=value2,...
-    var arr = [];
-  
-    for (var key in ob) {
-      arr.push(key + "=" + ob[key]);
-    }
-  
-    return arr.toString();
-  }
-  
-  var orm = {
-    all: function(tableInput, cb) {
-      var queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    // values is an array of values that we want to save to cols
-    // columns are the columns we want to insert the values into
-    create: function(table, cols, vals, cb) {
-      var queryString = "INSERT INTO " + table;
-  
-      queryString += " (";
-      queryString += cols.toString();
-      queryString += ") ";
-      queryString += "VALUES (";
-      queryString += printQuestionMarks(vals.length);
-      queryString += ") ";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, vals, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    // objColVals would be the columns and values that you want to update
-    // an example of objColVals would be {name: panther, sleepy: true}
-    update: function(table, objColVals, condition, cb) {
-      var queryString = "UPDATE " + table;
-  
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      console.log(queryString);
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    }
-  };
+// 
+const orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+// insert
+insertOne: function() {
+  let querySearch
+}
+
+// update
+updateOne: function() {
+
+}
+
+// delete
+deleteOne: function() {
+
+}
 
 // exporting orm variable as defined above
 module.exports = orm;
